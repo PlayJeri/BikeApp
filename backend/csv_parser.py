20c185da9498461f5bbd014180e7ef6f14899582
PlayJeri <numri.jeri@gmail.com>
2023-04-01 13:36:02 +0300

    added csv_parser script for populating db

---
+++ backend/csv_parser.py
@@ -0,0 +1,70 @@
+from app.database import SessionLocal, engine, Base
+from app.models import Journey, Station
+from datetime import datetime
+import csv
+
+
+date_format = "%Y-%m-%dT%H:%M:%S"
+journeys_csv_filenames = ['2021-05.csv', '2021-06.csv', '2021-07.csv']
+stations_csv_filename = 'stations.csv'
+csv_folder_path = 'csv_files'
+
+
+def add_journeys(folder_path, filename):
+    db = SessionLocal()
+    with open(f'{folder_path}/{filename}', 'r') as file:
+        reader = csv.reader(file)
+        next(reader) # skip the headers row
+        error_counter = 0
+        for row in reader:
+            try:
+                if (int(row[6]) > 10) and (int(row[7]) > 10):
+                    new_ride = Journey(
+                        departure_time=datetime.strptime(row[0], date_format),
+                        return_time=datetime.strptime(row[1], date_format),
+                        departure_station_id=int(row[2]),
+                        departure_station_name=row[3],
+                        return_station_id=int(row[4]),
+                        return_station_name=row[5],
+                        covered_distance=int(row[6]),
+                        duration=int(row[7])
+                        )
+                    db.add(new_ride)
+            except:
+                error_counter+=1
+        print(f'{error_counter} rows of data discarded')        
+        db.commit()
+
+def add_stations(folder_path, filename):
+    db = SessionLocal()
+    with open(f'{folder_path}/{filename}', 'r') as file:
+        reader = csv.reader(file)
+        next(reader) # skip the headers row
+        error_counter = 0
+        for row in reader:
+            try:
+                new_station = Station(
+                        station_id = int(row[1]),
+                        station_name_finnish = row[2],
+                        station_name_swedish = row[3],
+                        station_name_english = row[4],
+                        address_finnish = row[5],
+                        address_swedish = row[6],
+                        city_finnish = row[7],
+                        city_swedish = row[8],
+                        operator = row[9],
+                        capacity = int(row[10]),
+                        x_coordinate = float(row[11]),
+                        y_coordinate = float(row[12]))
+                db.add(new_station)
+            except:
+                error_counter+=1
+        print(f'{error_counter} rows of data discarded')
+        db.commit()
+            
+
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
+    add_stations(csv_folder_path, stations_csv_filename)
+    for file in journeys_csv_filenames:
+        add_journeys(csv_folder_path, file)
