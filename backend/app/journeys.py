360935bd35bd5ed7be75ddf088aed0dfdffeb9a2
PlayJeri <numri.jeri@gmail.com>
2023-04-01 20:23:11 +0300

    added journeys router and all journeys endpoint

---
+++ backend/app/journeys.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, HTTPException, Depends
+from database import get_db
+from sqlalchemy.orm import Session
+from models import Journey
+from schemas import JourneyResponse
+from typing import List
+
+
+router = APIRouter(
+    tags=['Journeys']
+)
+
+
+@router.get('/journeys')
+def get_journeys(db: Session = Depends(get_db)):
+    
+    journey_list = db.query(Journey).limit(10).all()
+
+    return journey_list
