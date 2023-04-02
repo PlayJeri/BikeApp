0971a0b21c2ec250aa0773b7a2a13f69610c4a06
PlayJeri <numri.jeri@gmail.com>
2023-04-02 16:53:46 +0300

    cors enabled

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import journeys, stations
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 app.include_router(journeys.router)
 app.include_router(stations.router)
