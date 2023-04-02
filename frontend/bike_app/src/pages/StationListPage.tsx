aee0ee418c3bddf73b46025f0a531b41cf75708e
PlayJeri <numri.jeri@gmail.com>
2023-04-02 16:59:10 +0300

    stationlist component and page added

---
+++ frontend/bike_app/src/pages/StationListPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+// import StationList from '../components/StationList';
+
+const StationListPage: React.FC = () => {
+  return (
+    <div className="bg-gray-900 min-h-screen text-white">
+      <Navbar />
+    </div>
+  );
+};
+
+export default StationListPage;
