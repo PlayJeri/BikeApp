587c9e39676cc2e15955f1026d1098fd4f5fef08
PlayJeri <numri.jeri@gmail.com>
2023-04-09 11:33:55 +0300

    added ordering by column

--- backend/app/routers/journeys.py
+++ backend/app/routers/journeys.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.orm import Session
+from sqlalchemy import desc
+from typing import Optional
 from fastapi_pagination import Page, paginate
 from ..database import get_db
 from ..models import Journey
@@ -12,15 +14,30 @@ router = APIRouter(
 
 
 @router.get('/journeys', response_model=Page[JourneyResponse])
-def get_journeys(limit: int = 10,
+def get_journeys(limit: int = 300,
+                 sort_by: Optional[str] = None,
+                 order: Optional[str] = None,
+                 page: int = 1,
                  offset: int = 0,
                  db: Session = Depends(get_db)):
+    
 
-    journey_list = db.query(Journey).limit(limit).offset(offset).all()
+    if sort_by == 'null' or order == 'null':
+        sort_by = None
+        order = None
+
+    query = db.query(Journey)
+
+    if sort_by:
+        if order == 'asc':
+            query = query.order_by(sort_by)
+        else:
+            query = query.order_by(desc(sort_by))
+
+    journey_list = query.limit(limit).offset(offset).all()
+
     if not journey_list:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Journeys not found")
+                            detail="No journeys found")
 
     return paginate(journey_list)
-
-
--- frontend/bike_app/src/components/JourneyList.tsx
+++ frontend/bike_app/src/components/JourneyList.tsx
@@ -16,28 +16,32 @@ type JourneysResponse = {
     pages: number
 }
 
+type SortBy = {
+  column: string | null
+  order: string | null
+}
+
 const JourneyList: React.FC = () => {
     const [journeys, setJourneys] = useState<Journey[]>([])
     const [total, setTotal] = useState<number>(1)
     const [page, setPage] = useState<number>(1)
     const [size, setSize] = useState<number>(30)
-    const [pages, setPages] = useState<number>(1)
+    const [totalPages, setTotalPages] = useState<number>(1)
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const [error, setError] = useState<string>('')
-
-    const limit = 300
+    const [sortBy, setSortBy] = useState<SortBy>({ column: null, order: null})
 
     useEffect(() => {
         const fetchJourneyList = async () => {
             setIsLoading(true)
                 try {
-                    const response = await fetch(`http://127.0.0.1:8000/journeys?limit=${limit}&page=${page}&size=${size}`)
+                    const response = await fetch(`http://127.0.0.1:8000/journeys?&sort_by=${sortBy.column}&order=${sortBy.order}&page=${page}`)
                     const data: JourneysResponse = await response.json()
                     setJourneys(data.items)
                     setTotal(data.total)
                     setPage(data.page)
                     setSize(data.size)
-                    setPages(data.pages)
+                    setTotalPages(data.pages)
                 } catch (error) {
                     setError('Error fetching data')
                 } finally {
@@ -45,7 +49,7 @@ const JourneyList: React.FC = () => {
                 }
         }
         fetchJourneyList()
-    }, [page])
+    }, [page, sortBy])
 
     const formatDuration = (durationInSeconds: number) => {
         const minutes = Math.floor(durationInSeconds / 60)
@@ -55,41 +59,93 @@ const JourneyList: React.FC = () => {
         return `${formattedMinutes}:${formattedSeconds}`
     }
 
+    const nextPage = () => {
+      if (page < totalPages) {
+          setPage(page + 1)
+      }
+  }
+  
+    const previousPage = () => {
+      if (page > 1) {
+          setPage(page - 1)
+      }
+  }
+
+  const handleSort = (column: string) => {
+    let order = 'asc'
+    if (sortBy.column === column && sortBy.order === 'asc') {
+      order = 'desc'
+    }
+    setSortBy({ column, order })
+  }
+
   return (
     <div>
-    <div className="shadow-md rounded my-6">
-      <table className="w-full sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-6/12 mx-auto bg-gray-100">
-        <thead>
-          <tr className="bg-gray-200 text-gray-800 uppercase text-sm leading-normal">
-            <th className="py-3 px-6 text-left">Departure Station</th>
-            <th className="py-3 px-6 text-left">Return Station</th>
-            <th className="py-3 px-6 text-center">Distance</th>
-            <th className="py-3 px-6 text-center">Duration</th>
-          </tr>
-        </thead>
-        <tbody className="text-gray-600 text-sm font-light">
-          {journeys.map((journey) => (
-            <tr
-              key={`${journey.id}`}
-              className="border-b border-gray-200 hover:bg-gray-200"
-            >
-              <td className="py-3 px-6 text-left whitespace-nowrap">
-                {journey.departure_station_name}
-              </td>
-              <td className="py-3 px-6 text-left whitespace-nowrap">
-                {journey.return_station_name}
-              </td>
-              <td className="py-3 px-6 text-center">
-                {(journey.covered_distance / 1000).toFixed(1)} KM
-              </td>
-              <td className="py-3 px-6 text-center">
-                {formatDuration(journey.duration)}
-              </td>
+      <div className="shadow-md rounded mt-6 pb-24">
+        <table className="w-full sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-6/12 mx-auto bg-gray-100">
+          <thead>
+            <tr className="bg-gray-200 text-gray-800 uppercase text-sm leading-normal">
+              <th onClick={() => handleSort('departure_station_name')} className="cursor-pointer py-3 px-6 text-left">
+                Departure Station
+              </th>
+              <th onClick={() => handleSort('return_station_name')} className="cursor-pointer py-3 px-6 text-left">
+                Return Station
+              </th>
+              <th onClick={() => handleSort('covered_distance')} className="cursor-pointer py-3 px-6 text-center">
+                Distance
+              </th>
+              <th onClick={() => handleSort('duration')} className="cursor-pointer py-3 px-6 text-center">
+                Duration
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody className="text-gray-600 text-sm font-light">
+            {journeys.map((journey) => (
+              <tr
+                key={`${journey.id}`}
+                className="border-b border-gray-200 hover:bg-gray-200"
+              >
+                <td className="py-3 px-6 text-left whitespace-nowrap">
+                  {journey.departure_station_name}
+                </td>
+                <td className="py-3 px-6 text-left whitespace-nowrap">
+                  {journey.return_station_name}
+                </td>
+                <td className="py-3 px-6 text-center">
+                  {(journey.covered_distance / 1000).toFixed(1)} KM
+                </td>
+                <td className="py-3 px-6 text-center">
+                  {formatDuration(journey.duration)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex justify-center py-4">
+        <button
+            className={`py-2 px-4 rounded-md shadow-md ${
+                page == 1 ? "hidden" : "bg-blue-800 hover:bg-blue-600 cursor-pointer"
+            }`}
+            onClick={previousPage}
+            disabled={page <= 1}
+        >
+            {`${page - 1}`}    
+        </button>
+        <button
+            className="py-2 px-4 mx-4 rounded-md shadow-md bg-blue-500 cursor-default">
+            {page}
+        </button>
+        <button
+            className={`py-2 px-4 rounded-md shadow-md ${
+                page >= totalPages ? "hidden" : "bg-blue-800 hover:bg-blue-400 cursor-pointer"
+            }`}
+            onClick={nextPage}
+            disabled={page >= totalPages}
+        >
+            {`${page + 1}`}    
+        </button>
+      </div>
   </div>
   )
 }
