6fc64983787b2d3f0be1012a7a15c90bc61a2e26
PlayJeri <numri.jeri@gmail.com>
2023-04-18 19:29:53 +0300

    added error handling

--- backend/app/routers/admin.py
+++ backend/app/routers/admin.py
@@ -33,13 +33,18 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@router.post('/station')
+@router.post('/station', status_code=status.HTTP_201_CREATED)
 def add_station(station_schema: schemas.Station ,db: Session = Depends(get_db), current_user = Depends(get_current_user)):
 
-    new_station = models.Station(**station_schema.dict())
-
-    db.add(new_station)
-    db.commit()
-    db.refresh(new_station)
+    try:
+        new_station = models.Station(**station_schema.dict())
+        db.add(new_station)
+        db.commit()
+        db.refresh(new_station)
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Form data invalid"
+        )
 
     return new_station
